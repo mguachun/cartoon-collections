8f886217317003232630007909f544031e06a36e
Melissa Guachun <1212malka@gmail.com>
2021-01-19 20:31:32 -0500

    commit tree b2b27079c175f453a164d6381e0a5c64e7b9d8ef

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,8 @@ end
 def find_the_cheese(array)
 i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  while i < cheese.types.length
-    return cheese_types[i] if array.include?(cheese_types[i])
+  while i < cheese_types.length
+    return cheese_types[i] if array.include? (cheese_types[i])
     i += 1
 end
 end
